b08e1979e3abb7d359ebcca8cc169decf4cae2af
IIMacGyverII <macgyverjunk@yahoo.com>
2026-02-03 16:06:11 -0600

    commented out the upgrade to pro button from the sidebar

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -1,9 +1,9 @@
 /**
 =========================================================
-* Material Home 2 React - v2.2.0
+* Material Dashboard 2 React - v2.2.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-home-react
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
 Coded by www.creative-tim.com
@@ -27,19 +27,19 @@ import Divider from "@mui/material/Divider";
 import Link from "@mui/material/Link";
 import Icon from "@mui/material/Icon";
 
-// Material Home 2 React components
+// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDButton from "components/MDButton";
 
-// Material Home 2 React example components
+// Material Dashboard 2 React example components
 import SidenavCollapse from "examples/Sidenav/SidenavCollapse";
 
 // Custom styles for the Sidenav
 import SidenavRoot from "examples/Sidenav/SidenavRoot";
 import sidenavLogoLabel from "examples/Sidenav/styles/sidenav";
 
-// Material Home 2 React context
+// Material Dashboard 2 React context
 import {
   useMaterialUIController,
   setMiniSidenav,
@@ -179,10 +179,11 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
         }
       />
       <List>{renderRoutes}</List>
+      {/*
       <MDBox p={2} mt="auto">
         <MDButton
           component="a"
-          href="https://www.creative-tim.com/product/material-home-pro-react"
+          href="https://www.creative-tim.com/product/material-dashboard-pro-react"
           target="_blank"
           rel="noreferrer"
           variant="gradient"
@@ -192,6 +193,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           upgrade to pro
         </MDButton>
       </MDBox>
+      */}
     </SidenavRoot>
   );
 }
