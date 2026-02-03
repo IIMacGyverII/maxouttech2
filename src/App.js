6dcaec9cbd9076ae840c6f0989e0fde372eea287
IIMacGyverII <macgyverjunk@yahoo.com>
2026-02-03 17:09:56 -0600

    changed the sidebar width and added animated logo

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ import routes from "routes";
 import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 // Images
-import brandWhite from "assets/images/logo-ct.png";
-import brandDark from "assets/images/logo-ct-dark.png";
+import brandWhite from "assets/images/animation/maxoutlogo2.webp";
+import brandDark from "assets/images/animation/maxoutlogo2.webp";
 
 export default function App() {
   const [controller, dispatch] = useMaterialUIController();
@@ -155,7 +155,7 @@ export default function App() {
             <Sidenav
               color={sidenavColor}
               brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-              brandName="Maxouttech"
+              //brandName="MaxOutTech"
               routes={routes}
               onMouseEnter={handleOnMouseEnter}
               onMouseLeave={handleOnMouseLeave}
@@ -179,7 +179,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Maxouttech"
+            //brandName="Maxouttech"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
